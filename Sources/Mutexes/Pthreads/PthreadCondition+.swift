4b6603f2388da12d39115481cadaff0628134e7b
Serhiy Butz <serhiybutz@gmail.com>
2022-11-02 01:07:52 -0400

    Bring everything in line with renaming the account.

--- Sources/Mutexes/Pthreads/PthreadCondition+.swift
+++ Sources/Mutexes/Pthreads/PthreadCondition+.swift
@@ -2,7 +2,7 @@
 //  PthreadCondition+.swift
 //  Mutexes
 //
-//  Created by Serge Bouts on 4/2/20.
+//  Created by Serhiy Butz on 4/2/20.
 //  Copyright © 2020 iRiZen.com. All rights reserved.
 //
 
